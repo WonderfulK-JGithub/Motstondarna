0312e139cf94b60bf4af7bd6203a65d7f2b49fa1
angulan <79506187+angulan@users.noreply.github.com>
2022-01-12 15:32:40 +0100

    Update PlatformMovement.cs

--- Motstondarna GruppProjekt/Assets/Scripts/PlatformMovement.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/PlatformMovement.cs
@@ -1,26 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    public Transform platform;
-    [Range(0, 5)]
-    public float speed;
-    public bool backAndForth;
-    bool reverse;
-    Transform[] children;
-    Transform[] points;
-    int targetPoint = 0;
-    public List<Transform> objectsOnPlatform = new List<Transform>();
+    Transform platform; // plattformen - Anton
+    [Range(0, 5)] // hastigheten ska inte vara för hög - Anton
+    public float speed; // hastigheten - Anton
+    public bool backAndForth; // om plattformen ska röra sig fram och tillbaka - Anton
+    bool reverse; // används endast om backAndForth är satt på true och plattformen åker tillbaka - Anton
+    Transform[] children; // får tag på alla children för att den senare ska extrahera ut punkterna - Anton
+    Transform[] points; // alla punkter (de berättar för plattformen hur den ska åka) - Anton
+    int targetPoint = 0; // vilken punkt plattan rör sig mot - Anton
+    public List<Transform> objectsOnPlatform = new List<Transform>(); // vilka objekt som nuddar plattformen - Anton
 
     private void Start()
     {
-        children = GetComponentsInChildren<Transform>();
-        points = new Transform[children.Length - 2];
+        children = GetComponentsInChildren<Transform>(); // får tag på alla children - Anton
+        platform = children[1]; // plattformen är första childen - Anton
+        points = new Transform[children.Length - 2]; // antalet punkter är points längd utan parenten och plattformen - Anton
         for (int i = 2; i < children.Length; i++)
         {
-            points[i - 2] = children[i];
+            points[i - 2] = children[i]; // lägger in punkterna i points-arrayen
         }
     }
     private void FixedUpdate()
