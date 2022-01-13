f8fb53391e0779a9abc8ae88c8da6ad08d5c64e9
angulan <79506187+angulan@users.noreply.github.com>
2022-01-13 16:08:07 +0100

    kommentering

--- Motstondarna GruppProjekt/Assets/Scripts/JumpPad.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/JumpPad.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     [Range(0, 20)]
-    public float strength;
-    private void OnCollisionEnter(Collision collision)
+    public float strength; // hur högt den ska hoppa - Anton
+    private void OnCollisionEnter(Collision collision) // känner av när något hoppar på den - Anton
     {
-        if (collision.gameObject.GetComponent<Rigidbody>() != null)
+        if (collision.gameObject.GetComponent<Rigidbody>() != null) // om objektet har en rigidbody - Anton
         {
-            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
-            rb.velocity = new Vector3(rb.velocity.x, strength, rb.velocity.z);
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>(); // referens till rigidbodyn - Anton
+            rb.velocity = new Vector3(rb.velocity.x, strength, rb.velocity.z); // skjuter upp objektet i luften - Anton
         }
     }
 }
