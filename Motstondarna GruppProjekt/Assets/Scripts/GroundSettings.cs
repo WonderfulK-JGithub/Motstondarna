5aa57819e28fdf16d933dbdc10b66605b081e4b3
angulan <79506187+angulan@users.noreply.github.com>
2022-01-17 16:07:48 +0100

    asxc

--- Motstondarna GruppProjekt/Assets/Scripts/GroundSettings.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/GroundSettings.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class GroundSettings : MonoBehaviour
 {
-    public List<Vector3> groundCoords = new List<Vector3>();
-    public Transform[] faces;
+    public bool wallCollision = false;
+    List<Vector3> groundCoords = new List<Vector3>();
+    Transform[] faces;
     void Start()
     {
         GroundSettings[] grounds = FindObjectsOfType<GroundSettings>();
@@ -13,6 +14,10 @@ public class GroundSettings : MonoBehaviour
             groundCoords.Add(ground.transform.position);
         }
         faces = GetComponentsInChildren<Transform>();
+        for (int i = 1; i < faces.Length; i++)
+        {
+            faces[i].GetComponent<MeshCollider>().enabled = wallCollision;
+        }
 
         if (groundCoords.Contains(transform.position + new Vector3(2, 0, 0))) { Destroy(faces[1].gameObject); }
         if (groundCoords.Contains(transform.position + new Vector3(-2, 0, 0))) { Destroy(faces[2].gameObject); }
