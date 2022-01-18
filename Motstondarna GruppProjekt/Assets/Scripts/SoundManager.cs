f8604258e2e35e7842d63373250b7654b2913772
TheoW04 <theo.wadsworth@elev.ga.lbs.se>
2022-01-18 13:48:59 +0100

    SFX (:
    Gjort klart nästan alla sfx behöver sätta in dom i spelet (:

--- Motstondarna GruppProjekt/Assets/Scripts/SoundManager.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/SoundManager.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
-    public static AudioClip Ljud; //Definerar som audiclip /Theo
+    public static AudioClip AmbientWind, Checkpoint, Click, Dash, JumpPad, KägglaDamage, Landa, PowerUp, Rolling, Ränna, Skada; //Definerar som audiclip /Theo
     static AudioSource audioSrc;
 
     void Start() //Parar ihop rätt variabel med rätt audioclip
     {
-        Ljud = Resources.Load<AudioClip>("Ljud");
+        AmbientWind = Resources.Load<AudioClip>("AmbientWind");
+        Checkpoint = Resources.Load<AudioClip>("Checkoint");
+        Click = Resources.Load<AudioClip>("Click");
+        Dash = Resources.Load<AudioClip>("Dash");
+        JumpPad = Resources.Load<AudioClip>("JumpPad");
+        KägglaDamage = Resources.Load<AudioClip>("KägglaDamage");
+        Landa = Resources.Load<AudioClip>("Landa");
+        PowerUp = Resources.Load<AudioClip>("PowerUp");
+        Rolling = Resources.Load<AudioClip>("Rolling");
+        Ränna = Resources.Load<AudioClip>("Ränna");
+        Skada = Resources.Load<AudioClip>("Skada");
 
         audioSrc = GetComponent<AudioSource>();
     }
@@ -22,11 +32,44 @@ public class SoundManagerScript : MonoBehaviour
     {
         switch (clip)
         {
-            case "Ljud":
-                audioSrc.PlayOneShot(Ljud);
+            case "AmbientWind":
+                audioSrc.PlayOneShot(AmbientWind);
                 break;
+            case "Checkpoint":
+                audioSrc.PlayOneShot(Checkpoint);
+                break;
+            case "Click":
+                audioSrc.PlayOneShot(Click);
+                break;
+            case "Dash":
+                audioSrc.PlayOneShot(Dash);
+                break;
+            case "JumpPad":
+                audioSrc.PlayOneShot(JumpPad);
+                break;
+            case "KägglaDamage":
+                audioSrc.PlayOneShot(KägglaDamage);
+                break;
+            case "Landa":
+                audioSrc.PlayOneShot(Landa);
+                break;
+            case "PowerUp":
+                audioSrc.PlayOneShot(PowerUp);
+                break;
+            case "Rolling":
+                audioSrc.PlayOneShot(Rolling);
+                break;
+            case "Ränna":
+                audioSrc.PlayOneShot(Ränna);
+                break;
+            case "Skada":
+                audioSrc.PlayOneShot(Skada);
+                break;
+
 
 
         }
     }
 }
+
+// För att spela ljuden SoundManagerScript.PlaySound ("InsertName"); /Theo
