90073c369fd464260838d7d083de4f8dda4dfcca
TheoW04 <theo.wadsworth@elev.ga.lbs.se>
2022-01-19 16:09:13 +0100

    Fixat click sfx och både menyernas buttons/slider

---
+++ Motstondarna GruppProjekt/Assets/Scripts/SettingsMenyScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenyScript : MonoBehaviour
+{
+    public void Settings() // Button till starta spelet
+    {
+        SceneManager.LoadScene("SettingsMeny");
+    }
+}
