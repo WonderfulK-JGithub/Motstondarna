be863d0710c2b6b41e1d40ca0273720d73579711
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2022-01-24 07:25:23 +0100

    o

--- Motstondarna GruppProjekt/Assets/Scripts/HubScripts/LevelSelectManager.cs
+++ Motstondarna GruppProjekt/Assets/Scripts/HubScripts/LevelSelectManager.cs
@@ -15,12 +15,15 @@ public class LevelSelectManager : MonoBehaviour
     [SerializeField] Color lockedColor;
 
     [SerializeField] float rotateSpeed;
+    [SerializeField] float transitionWait;
 
     HubCamera cam;
 
     int levelIndex;
 
     public int levelProgress;
+
+    bool hasSelected;
 
     void Awake()
     {
@@ -46,35 +49,55 @@ public class LevelSelectManager : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if(!hasSelected)
         {
-            levelIndex++;
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            levelIndex--;
-        }
-
-        levelIndex = Mathf.Clamp(levelIndex, 0, levelCameraPoints.Length - 1);
-
-        cam.targetPoint = levelCameraPoints[levelIndex];
-
-        levelNumbers[levelIndex].transform.localRotation *= Quaternion.Euler(0f, rotateSpeed * Time.deltaTime,0f);
-
-        ramp.transform.position = levelCameraPoints[levelIndex].position - new Vector3(rampDistance,0.5f,0f);
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            foreach (var item in levelNumbers)
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                item.enabled = false;
+                levelIndex++;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                levelIndex--;
             }
 
-            BallMovement ball = FindObjectOfType<BallMovement>();
-            ball.state = PlayerState.Hub;
-            ball.rb.velocity = new Vector3(11f, 0f, 0f);
+            levelIndex = Mathf.Clamp(levelIndex, 0, levelCameraPoints.Length - 1);
+
+            cam.targetPoint = levelCameraPoints[levelIndex];
+
+            levelNumbers[levelIndex].transform.localRotation *= Quaternion.Euler(0f, rotateSpeed * Time.deltaTime, 0f);
+
+            ramp.transform.position = levelCameraPoints[levelIndex].position - new Vector3(rampDistance, 0.5f, 0f);
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (levelIndex <= levelProgress)
+                {
+                    LevelSelected();
+                }
+            }
         }
+        
     }
 
+    void LevelSelected()
+    {
+        foreach (var item in levelNumbers)
+        {
+            item.enabled = false;
+        }
+
+        BallMovement ball = FindObjectOfType<BallMovement>();
+        ball.state = PlayerState.Hub;
+        ball.rb.velocity = new Vector3(11f, 0f, 0f);
+
+        hasSelected = true;
+
+        Invoke("EnterLevel", transitionWait);
+    }
+
+    void EnterLevel()
+    {
+        SceneTransition.current.EnterScene(levelIndex + 0);
+    }
     
 }
